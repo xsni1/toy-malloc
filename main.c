cd1fbeb9a99b8c93e83bf1d3dc22a4bbdad70b2c
xsni1 <thesniboy@gmail.com>
2024-05-12 14:09:03 +0200

    tmalloc_print method for debuging purposes

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#define BLOCK_META_SIZE sizeof(struct block_meta)
+
 struct block_meta {
   size_t size;
   struct block_meta *next;
@@ -9,29 +11,54 @@ struct block_meta {
 
 void *HEAD = NULL;
 
+void *request_space(int n) {
+  struct block_meta *cur = sbrk(0);
+  void *allocated = sbrk(n + BLOCK_META_SIZE);
+
+  if (allocated == (void *)-1) {
+    return NULL;
+  }
+
+  cur->next = NULL;
+  cur->free = 0;
+  cur->size = n;
+  return cur;
+}
+
 void *tmalloc(int n) {
   if (HEAD == NULL) {
-    struct block_meta *cur = sbrk(0);
-    void *allocated = sbrk(n);
-
-    if (allocated == (void *)-1) {
-      return NULL;
-    }
-
-    cur->next = NULL;
-    cur->free = 0;
-    cur->size = n;
+    void *cur = request_space(n);
     HEAD = cur;
 
     return cur + 1;
   }
 
-  return NULL;
+  // TODO: look for already free space
+  struct block_meta *cur = HEAD;
+  while (cur->next) {
+    cur = cur->next;
+  }
+  void *new_block = request_space(n);
+  cur->next = new_block;
+
+  return cur + 1;
+}
+
+void tmalloc_print() {
+  struct block_meta *cur = HEAD;
+  while (cur) {
+    printf("[address=%p size=%zu free=%d]\n", cur, cur->size, cur->free);
+    cur = cur->next;
+  }
 }
 
 int main() {
   void *addr = tmalloc(1);
-  /* printf("block_meta size=%lu. addr: %p\n", sizeof(struct block_meta), addr); */
-  while (1) {}
+  while (1) {
+    getchar();
+    void *addr = tmalloc(1000);
+    tmalloc_print();
+    getchar();
+  }
   return 0;
 }
